1953178b486893840e0ff23318bb61dbc71d2379
shlokburmi <135860207+shlokburmi@users.noreply.github.com>
2026-02-22 16:13:12 +0530

    comments

--- Easy/136. Single Number.cpp
+++ Easy/136. Single Number.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans=0;
+        int ans=0; // iterating thorugh whole array , XOR of all elements in array and unqiue value prints
         for(int i:nums){
-            ans=ans^i;
+            ans=ans^i;  // ^ is XOR function n ^ n =0 and n ^ 0 = n 
         }
         return ans;
 
